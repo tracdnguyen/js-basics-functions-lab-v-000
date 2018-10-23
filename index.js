98ad71faf6190dfee7c046835893b6b9b70aaacf
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-23 16:31:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(distance) {
   if (distance < 42) {
-    fromHQ = distance + 42;
+    fromHQ = 42 - distance;
     return fromHQ;
   }
   else if (distance > 42) {
