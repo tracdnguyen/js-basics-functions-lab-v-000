9a7ad5521bcdee3c84007244c1d5643c9e5654ef
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-23 17:03:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  if (distance < 42) {
+    fromHQ = 42 - distance;
+    return fromHQ;
+  }
+  else if (distance > 42) {
+    fromHQ = distance - 42;
+    return fromHQ;
+  }
+}
+
+function distanceFromHqInFeet(distance) {
+  blocks = distanceFromHqInBlocks(distance);
+  return blocks * 264
+}
+
+function distanceTravelledInFeet(someValue1, someValue2) {
+  if (someValue1 > someValue2) {
+    someValue3 = someValue1 - someValue2;
+    return someValue3 * 264;
+  }
+  else if (someValue1 < someValue2) {
+    someValue3 = someValue2 - someValue1;
+    return someValue3 * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet(start, destination) <= 400)
+    return 0;
+  else if (distanceTravelledInFeet(start, destination) <= 2000)
+    return .02 * (distanceTravelledInFeet(start, destination) - 400);
+  else if (distanceTravelledInFeet(start, destination) <= 2500)
+    return 25;
+  else
+    return 'cannot travel that far';
+}
